63ec797e89c657025c8e087c38db694a508b200d
USER <USER@Susana>
2024-02-13 00:05:27 +0100

    modificado pom despues de refactorizar nombre paquete añadido metodo operativa_cuenta añadido nuevo parametro cantidad

--- src/main/java/com/mycompany/cuentas/Main.java
+++ src/main/java/com/mycompany/cuentas/Main.java
@@ -13,10 +13,10 @@ public class Main {
 
     public static void main(String[] args) {
         CCuenta cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        operativa_cuenta(cuenta1, 2300, 695);
+        operativa_cuenta(cuenta1, 2300, 695, null);
     }
 
-    private static void operativa_cuenta(CCuenta cuenta1, double retirar, double ingresar) {
+    private static Void operativa_cuenta(CCuenta cuenta1, double retirar, double ingresar, float cantidad) {
         double saldoActual;
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es" + saldoActual);
