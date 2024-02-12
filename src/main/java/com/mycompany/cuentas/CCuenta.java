f76d48b57ec39021bb7b184260f4989fd6b3559b
Susana <>
2024-02-13 00:42:02 +0100

    encapsular atributos clase CCuenta (getter y setter)

--- src/main/java/com/mycompany/cuentas/CCuenta.java
+++ src/main/java/com/mycompany/cuentas/CCuenta.java
@@ -6,7 +6,7 @@ package com.mycompany.cuentas;
 
 /**
  *
- * @author USER
+ * @author Susana
  */
 public class CCuenta {
     
@@ -27,16 +27,72 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -45,7 +101,7 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
     
 }
