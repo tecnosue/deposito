2f4230156787c97450585f5c513e1ff1b6dbda70
USER <USER@Susana>
2024-02-13 00:33:59 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/cuentas/CCuenta.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cuentas;
+
+/**
+ *
+ * @author USER
+ */
+public class CCuenta {
+    
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta()
+    {
+    }
+    
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    public double estado()
+    {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+    
+}
--- src/main/java/com/mycompany/cuentas/Main.java
+++ src/main/java/com/mycompany/cuentas/Main.java
@@ -13,14 +13,14 @@ public class Main {
 
     public static void main(String[] args) {
         CCuenta cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        operativa_cuenta(cuenta1, 2300, 695, null);
+        operativa_cuenta(cuenta1, 2300, 695,30);
     }
 
-    private static Void operativa_cuenta(CCuenta cuenta1, double retirar, double ingresar, float cantidad) {
+    private static void operativa_cuenta(CCuenta cuenta1, double retirar, double ingresar, float cantidad) {
         double saldoActual;
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es" + saldoActual);
-
+        System.out.println("cantidad es " + cantidad);
         try {
             cuenta1.retirar(retirar);
         } catch (Exception e) {
