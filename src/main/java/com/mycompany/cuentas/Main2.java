52bc7a0d089240c7c3c8a1d5663faa556c8b9956
USER <USER@Susana>
2024-02-16 23:44:06 +0100

    refactorizacion puntos 1,2 y 4

--- src/main/java/com/mycompany/cuentas/Main2.java
+++ src/main/java/com/mycompany/cuentas/Main2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pruebaREfactorizacion;
+package com.mycompany.cuentas;
 
 import com.mycompany.cuentas.CCuenta;
 
@@ -13,21 +13,21 @@ import com.mycompany.cuentas.CCuenta;
 public class Main2 {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
