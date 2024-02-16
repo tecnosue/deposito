389ac60dba273c8c8472046499bbb3c382334d74
USER <USER@Susana>
2024-02-16 20:25:43 +0100

    encapsular atributos clase CCuenta (getter y setter)

---
+++ src/main/java/com/mycompany/cuentas/Main2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cuentas;
+
+/**
+ *
+ * @author USER
+ */
+public class Main2 {
+
+    public static void main(String[] args) {
+        CCuenta miCuenta;
+        double saldoActual;
+
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            miCuenta.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            miCuenta.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
